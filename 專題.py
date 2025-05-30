599527840a9b8c1a725868bc644d857efc8d5ed7
ericeeic <110375075+ericeeic@users.noreply.github.com>
2025-05-30 11:16:48 +0800

    Add files via upload

--- 專題.py
+++ 專題.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import google.generativeai as genai
+genai.configure(api_key="AIzaSyAP7BSVTOBJo2CDpincq7dAlTmDG4Ix5c0")
 CSV1 = st.file_uploader("FILE:", accept_multiple_files=True,type=['csv','txt','jpg','png'])
 for i in CSV1:
    if i.name.endswith(('.jpg', '.jpeg', '.png')):
@@ -9,4 +11,13 @@ for i in CSV1:
        st.write("filename:", i.name)
        x=pd.read_csv(i ,encoding="BIG5")
        st.dataframe(x)
-      
+st.title("Gemini")
+
+model = genai.GenerativeModel("models/gemini-1.5-flash")
+chat = genai.ChatSession(model=model)
+
+user_input = st.text_input("請輸入問題")
+
+if user_input:
+    response = chat.send_message(user_input)
+    st.write(response.text)
